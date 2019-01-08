f2cf22e2a445afd47e9a60dc3237314bb7490a2b
Steven Berg <steven@stevenberg.net>
2019-01-08 11:16:35 -0500

    Update iTerm2 shell integration

--- tag-mac/config/iterm2/shell_integration.fish
+++ tag-mac/config/iterm2/shell_integration.fish
@@ -1,4 +1,18 @@
-if begin; status --is-interactive; and not functions -q -- iterm2_status; and [ "$TERM" != screen ]; end
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+if begin; status --is-interactive; and not functions -q -- iterm2_status; and [ "$ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX""$TERM" != screen ]; end
   function iterm2_status
     printf "\033]133;D;%s\007" $argv
   end
@@ -30,7 +44,7 @@ if begin; status --is-interactive; and not functions -q -- iterm2_status; and [ 
 
   # iTerm2 inform terminal that command starts here
   function iterm2_precmd
-    printf "\033]1337;RemoteHost=%s@%s\007\033]1337;CurrentDir=$PWD\007" $USER $iterm2_hostname
+    printf "\033]1337;RemoteHost=%s@%s\007\033]1337;CurrentDir=%s\007" $USER $iterm2_hostname $PWD
 
     # Users can define a function called iterm2_print_user_vars.
     # It should call iterm2_set_user_var and produce no other output.
@@ -74,9 +88,13 @@ if begin; status --is-interactive; and not functions -q -- iterm2_status; and [ 
 
   # If hostname -f is slow for you, set iterm2_hostname before sourcing this script
   if not set -q iterm2_hostname
-    set iterm2_hostname (hostname -f)
+    set iterm2_hostname (hostname -f 2>/dev/null)
+    # some flavors of BSD (i.e. NetBSD and OpenBSD) don't have the -f option
+    if test $status -ne 0
+      set iterm2_hostname (hostname)
+    end
   end
 
   iterm2_precmd
-  printf "\033]1337;ShellIntegrationVersion=5;shell=fish\007"
+  printf "\033]1337;ShellIntegrationVersion=6;shell=fish\007"
 end
