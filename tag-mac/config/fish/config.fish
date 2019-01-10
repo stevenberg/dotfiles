6e400dfa72079322cdf869f58ec7f937ba9817db
Steven Berg <steven@stevenberg.net>
2019-01-09 21:25:14 -0500

    Improve environment variable declarations
    - Sort them alphabetically
    - Quote values

--- tag-mac/config/fish/config.fish
+++ tag-mac/config/fish/config.fish
@@ -1,17 +1,17 @@
-set -x HOMEBREW_INSTALL_CLEANUP 1
 set -x BROWSER 'open'
 set -x CDPATH . "$HOME/dev/repos"
 set -x EDITOR 'vim'
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow'
 set -x GOPATH "$HOME/go"
-set -x GOROOT "/usr/local/opt/go/libexec"
+set -x GOROOT '/usr/local/opt/go/libexec'
 set -x GREP_OPTIONS '--color=auto'
+set -x HOMEBREW_INSTALL_CLEANUP 1
 set -x LC_ALL 'en_US.UTF-8'
 set -x LESS '-F -g -i -R -w -X -z-3'
 set -x LESSOPEN '|lesspipe.sh %s'
 set -x LESS_ADVANCED_PREPROCESSOR 1
 set -x VISUAL $EDITOR
-set -x RUBY_CONFIGURE_OPTS --with-jemalloc
+set -x RUBY_CONFIGURE_OPTS '--with-jemalloc'
 
 umask 022
 
