154336a428ceeb58f2a2ca8838fd945764211a18
Steven Berg <steven@stevenberg.net>
2019-12-13 09:07:48 -0500

    Turn php function into a shell script
    So it will be used by PHP shell scripts with a shebang like
    `#!/usr/bin/env php`.

--- tag-mac/config/fish/functions/php.fish
+++
@@ -1,9 +0,0 @@
-function php --description 'Run the php command for the version specified in $PHP_VERSION'
-    if set -q PHP_VERSION
-        and test -n "$PHP_VERSION" -a -x /usr/local/opt/php@"$PHP_VERSION"/bin/php
-        /usr/local/opt/php@"$PHP_VERSION"/bin/php $argv
-    else
-        command php $argv
-    end
-end
-
