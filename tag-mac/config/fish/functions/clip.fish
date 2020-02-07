66250ce5e64d4838753b4c05f4825a8c9975c57e
Steven Berg <steven@stevenberg.net>
2020-02-07 14:35:38 -0500

    Fix clip
    - Ensure the actual cat command is used
    - Support copying JSON files

--- tag-mac/config/fish/functions/clip.fish
+++ tag-mac/config/fish/functions/clip.fish
@@ -1,6 +1,6 @@
 function clip
-    if file -I $argv[1] | grep -q text
-        cat "$argv" | pbcopy
+    if file -I $argv[1] | grep -q '(text|json)'
+        command cat "$argv" | pbcopy
     else
         echo "File \"$argv[1]\" is not plaintext." >&2
         return 1
