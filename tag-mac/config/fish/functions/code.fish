73a7c19416fcc503a815a90f0f3aa71534a7bee1
Steven Berg <steven@stevenberg.net>
2020-12-09 22:00:35 -0500

    Make code command open the current directory by default

--- tag-mac/config/fish/functions/code.fish
+++ tag-mac/config/fish/functions/code.fish
@@ -1,3 +1,9 @@
 function code -d 'Open a path in Visual Studio Code'
-    open "$argv" -b com.microsoft.VSCode
+    set -l path
+    if test (count $argv) = 0
+        set path '.'
+    else
+        set path "$argv"
+    end
+    open "$path" -b com.microsoft.VSCode
 end
