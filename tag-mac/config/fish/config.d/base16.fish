ef076f3d1497e3d10b766f3d14fceadeb36ef115
Steven Berg <steven@stevenberg.net>
2022-06-07 20:30:14 -0400

    Update base16 script

--- tag-mac/config/fish/config.d/base16.fish
+++ tag-mac/config/fish/config.d/base16.fish
@@ -12,7 +12,7 @@ if status --is-interactive
             sh $SCRIPT
             ln -sf $SCRIPT ~/.base16_theme
             set -x BASE16_THEME (string split -m 1 '-' $THEME)[2]
-            echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >  ~/.vimrc_background
+            echo -e "if !exists('g:colors_name') || g:colors_name != '$THEME'\n  colorscheme $THEME\nendif" >~/.vimrc_background
         end
     end
 end
