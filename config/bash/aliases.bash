d7173fa49104e522008362fdf80b64d66432ae63
Steven Berg <steven@stevenberg.net>
2022-07-04 12:55:30 -0400

    Update bash configuration

--- config/bash/aliases.bash
+++
@@ -1,4 +0,0 @@
-alias grep='grep --color=auto'
-alias hub='git'
-alias ll='ls -l'
-alias la='ll -a'
--- tag-mac/config/bash/mac.bash
+++
@@ -1,6 +0,0 @@
-export BROWSER='open'
-export GOPATH="$HOME/Development/go"
-export GOROOT='/usr/local/opt/go/libexec'
-export HOMEBREW_INSTALL_CLEANUP=1
-
-alias ls='ls -G'
