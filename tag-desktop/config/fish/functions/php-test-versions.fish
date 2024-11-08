cbf4fd04a9b86e33de51e7f3c75c516ebf5aa197
Steven Berg <steven@stevenberg.net>
2024-11-08 12:40:37 -0500

    Update PHP-related commands

--- tag-desktop/config/fish/functions/php-test-versions.fish
+++ tag-desktop/config/fish/functions/php-test-versions.fish
@@ -1,6 +1,8 @@
 function php-test-versions --description 'Run PHPUnit tests for multiple PHP versions'
     for v in (string split ' ' $PHP_VERSIONS)
-        set v (asdf latest php $v)
+        if string match --quiet --regex '^\d+\.\d+$' $v
+            set v (asdf latest php $v)
+        end
         echo "*** $v ***"
         asdf shell php $v
         trash vendor composer.lock
--- tag-desktop/config/fish/functions/phpstan.fish
+++ tag-desktop/config/fish/functions/phpstan.fish
@@ -2,6 +2,6 @@ function phpstan --description 'Run PHPStan with default options'
     if test (count $argv) -gt 0
         command phpstan $argv
     else
-        command phpstan --memory-limit=256M -v
+        command phpstan --memory-limit=1G -v
     end
 end
