1021adb2d47c1b668beb99a100d51aa22562a678
Steven Berg <steven@stevenberg.net>
2025-05-05 23:18:48 -0400

    Update Excel serverconfig completions

--- tag-excel/config/fish/completions/deploy.fish
+++ tag-excel/config/fish/completions/deploy.fish
@@ -1,3 +1,6 @@
 set -l sites (ls ./sites/)
 complete -c deploy -f
 complete -c deploy -n "not __fish_seen_subcommand_from $sites" -a "$sites"
+complete -c deploy -s h -l help
+complete -c deploy -s d -l debug
+complete -c deploy -s m -l mode -x -a 'app web full'
