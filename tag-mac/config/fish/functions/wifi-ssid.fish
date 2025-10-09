be2ab31db5dd2ae322c2ead21d65a5e56abaf98b
Steven Berg <steven@stevenberg.net>
2025-10-08 21:22:36 -0400

    Fix wifi-ssid

--- tag-mac/config/fish/functions/wifi-ssid.fish
+++ tag-mac/config/fish/functions/wifi-ssid.fish
@@ -1,3 +1,3 @@
 function wifi-ssid -d 'Print the SSID of the current wifi network'
-    wifi-interface | xargs networksetup -getairportnetwork | sed 's/Current Wi-Fi Network: //'
+    wifi-interface | xargs ipconfig getsummary | rg ' SSID' | cut -d : -f 2 | trim
 end
